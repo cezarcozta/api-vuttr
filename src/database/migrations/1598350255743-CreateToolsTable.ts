46ee52274862d4648b0ce08554d70acc67b53a8a
César Augusto Costa <cezarcozta@gmail.com>
2020-08-25 07:51:46 -0300

    eslint check

--- src/database/migrations/1598350255743-CreateToolsTable.ts
+++ src/database/migrations/1598350255743-CreateToolsTable.ts
@@ -2,48 +2,48 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export default class CreateToolsTable1598350255743 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-            new Table({
-                name: 'tools',
-                columns: [
-                    {
-                        name: 'id',
-                        type: 'uuid',
-                        isPrimary: true,
-                        generationStrategy: 'uuid',
-                        default: 'uuid_generate_v4()',
-                    },
-                    {
-                        name: 'name',
-                        type: 'varchar',
-                        isUnique: true,
-                    },
-                    {
-                        name: 'link',
-                        type: 'varchar',
-                    },
-                    {
-                        name: 'description',
-                        type: 'varchar',
-                    },
-                    {
-                        name: 'created_at',
-                        type: 'timestamp',
-                        default: 'now()',
-                    },
-                    {
-                        name: 'updated_at',
-                        type: 'timestamp',
-                        default: 'now()',
-                    },
-                ]
-            }),
-        );
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tools',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'link',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ]
+      }),
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('tools');
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tools');
+  }
 
 }
