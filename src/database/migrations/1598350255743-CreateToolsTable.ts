dce6ccb155bc82f46c4236f75ba63b16362fdf61
César Augusto Costa <cezarcozta@gmail.com>
2020-08-25 07:34:57 -0300

    init: migrationTest

--- src/database/migrations/1598350255743-CreateToolsTable.ts
+++ src/database/migrations/1598350255743-CreateToolsTable.ts
@@ -1,11 +1,49 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateToolsTable1598350255743 implements MigrationInterface {
+export default class CreateToolsTable1598350255743 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'tools',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'link',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'description',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ]
+            }),
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('tools');
     }
 
 }
